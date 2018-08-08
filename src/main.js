14d39c00d6cb4dfe721bbff3c50f47a691af7768
Hedim Ramirez <ody360@gmail.com>
2018-08-08 16:00:42 -0700

    working on login auth

--- src/main.js
+++ src/main.js
@@ -1,29 +1,32 @@
 window.baseURL = 'http://localhost:5000'
 
 const loginButton = document.getElementById('login-button')
+const logoutButton = document.getElementById('logout-button')
 const registerButton = document.getElementById('register-button')
 const templates = require('./templates/templates')
 const searchButton = document.getElementById('button-search')
+const loginPage = require('./render/login')
 
 const renderHomePage = require('./render/home')
 
-loginButton.addEventListener('click', loginForm)
-registerButton.addEventListener('click', registerForm)
 // searchButton.addEventListener('click', () => {
 //   window.location.hash = '#search'
 // })
 
 function homeView () {
-  renderHomePage.allMovies()
+  const token = localStorage.getItem('token')
+  if (!token) {
+        // Render homepage view only if there is no token.
+        // renderHome()
+    renderHomePage.allMovies()
+    loginButton.addEventListener('click', loginPage.createLogin())
+    registerButton.addEventListener('click', registerForm)
+  } else {
+        // renderHome(userId)
 
-    // const token = localStorage.getItem('token')
-    // if (!token) {
-    //     // Render homepage view only if there is no token.
-    //     // renderHome()
-    // } else {
-    //     // renderHome(userId)
-    // }
-
+      renderHomePage.allMovies()
+    }
+  
 }
 
 homeView()
@@ -50,19 +53,19 @@ function renderMain(){
   document.querySelector('main').classList.remove('d-none')
 }
 
-function loginForm() {
-  document.querySelector('title').textContent = 'Sign In'
-  document.querySelector('main').classList.add('d-none')
+// function loginForm() {
+//   document.querySelector('title').textContent = 'Sign In'
+//   document.querySelector('main').classList.add('d-none')
 
-  const container = document.getElementById('form-container')
-  container.innerHTML = templates.loginTemplate()
+//   const container = document.getElementById('form-container')
+//   container.innerHTML = templates.loginTemplate()
 
-  const cancelLogin = document.getElementById('cancel-login')
-  cancelLogin.addEventListener('click', renderMain)
+//   const cancelLogin = document.getElementById('cancel-login')
+//   cancelLogin.addEventListener('click', renderMain)
 
-  // const loginFormSubmit = document.getElementById('login')
-  // loginForm.addEventListener('submit', verify)
-}
+//   // const loginFormSubmit = document.getElementById('login')
+//   // loginForm.addEventListener('submit', verify)
+// }
 
 function registerForm() {
   document.querySelector('title').textContent = 'Register'
