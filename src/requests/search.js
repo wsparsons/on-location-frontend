b8d0e7886468781f049b8638f980dd2a2380e2e3
craig <grendel@shmoo.com>
2018-08-08 17:11:26 -0700

    Search functions working with minimal bugs.

--- src/requests/search.js
+++ src/requests/search.js
@@ -1,15 +1,25 @@
 const renderAllMovie = require('../render/home').renderAllMovie
+const renderSearchBar = require('../render/search').renderSearchBar
 
+async function localSearch(movieTitle) {
+  try {
+    return axios.get(`${baseURL}/api/movies/search?title=${movieTitle}`)
+      .then(
+        response => {
+          if (response.data.data.length === 0) {
 
-
-function localSearch(movieTitle) {
-
-  return axios.get(`${baseURL}/api/movies/search?title=${movieTitle}`)
-    .then(
-      response => {
-        renderAllMovie(response.data.data)
-      }
-    )
+            document.getElementById('noresults-alert').classList.remove('d-none')
+            
+          }
+          if (response.data.data.length >= 1) {
+            document.getElementById('noresults-alert').classList.add('d-none')
+          }
+          renderAllMovie(response.data.data)
+        }
+      )
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 function omdbSearch() {
